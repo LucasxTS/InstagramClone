7b5a3f9ec1e8536df0bdea23948ee1a53cb7cfaf
Lucas <lucastorres6431@gmail.com>
2024-01-13 15:27:35 -0300

    searcScreen images added

--- app/src/main/java/com/example/instagramclone/ui/views/search/SearchScreen.kt
+++ app/src/main/java/com/example/instagramclone/ui/views/search/SearchScreen.kt
@@ -1,12 +1,21 @@
 package com.example.instagramclone.ui.views.search
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.instagramclone.ui.views.search.components.ImagesSearchFeed
+import com.example.instagramclone.ui.views.search.components.SearchBar
+import com.example.instagramclone.ui.views.search.components.items
+
 
 @Composable
 fun SearchScreen(navController: NavController) {
+    Column {
+        SearchBar()
+        ImagesSearchFeed(list = items)
+    }
 
 }
 
--- app/src/main/java/com/example/instagramclone/ui/views/search/components/SearchImagens.kt
+++ app/src/main/java/com/example/instagramclone/ui/views/search/components/SearchImagens.kt
@@ -1,42 +1,71 @@
 package com.example.instagramclone.ui.views.search.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.instagramclone.R
 
+val items = listOf(
+    R.drawable.defante,
+    R.drawable.caaaalango,
+    R.drawable.igao,
+    R.drawable.trizpariz,
+    R.drawable.zeraofoto,
+    R.drawable.defante,
+    R.drawable.caaaalango,
+    R.drawable.igao,
+    R.drawable.trizpariz,
+    R.drawable.zeraofoto,
+    R.drawable.defante,
+    R.drawable.caaaalango,
+    R.drawable.igao,
+    R.drawable.trizpariz,
+    R.drawable.zeraofoto,
+    R.drawable.defante,
+    R.drawable.caaaalango,
+    R.drawable.igao,
+    R.drawable.trizpariz,
+    R.drawable.zeraofoto,
+    R.drawable.defante,
+    R.drawable.caaaalango,
+    R.drawable.igao,
+    R.drawable.trizpariz,
+    R.drawable.zeraofoto,
+    R.drawable.defante,
+    R.drawable.caaaalango,
+    R.drawable.igao,
+    R.drawable.trizpariz,
+    R.drawable.zeraofoto
+)
+
 @Composable
-fun ImagesSearchFeed() {
-    val items = listOf(
-        R.drawable.defante,
-        R.drawable.caaaalango,
-        R.drawable.igao,
-        R.drawable.trizpariz
-    )
+fun ImagesSearchFeed(list: List<Int>) {
+
 
    LazyVerticalGrid(
        columns = GridCells.Fixed(3),
        contentPadding = PaddingValues(8.dp),
        modifier = Modifier
-           .fillMaxSize()
+           .fillMaxWidth()
+           .background(Color.Black)
    ) {
     items(items.size) {
         Box(
             modifier = Modifier
-                .padding(2.dp)
-                .size(200.dp)
+                .padding(4.dp)
         ) {
-            Image(painter = painterResource(id = it), contentDescription = null)
+            Image(painter = painterResource(id = list[it]), contentDescription = null)
         }
     }
    }
@@ -46,5 +75,5 @@ fun ImagesSearchFeed() {
 @Preview
 @Composable
 fun ImageSearchFeedPreview() {
-    ImagesSearchFeed()
+    ImagesSearchFeed(items)
 }
